55a6ea85f441af6e6f04797d0baae34c238a81b1
Kathleen Borg <kborg@kborg-ltm1.internal.salesforce.com>
2018-06-16 12:11:05 -0700

    push seeds files

---
+++ posts.sql
@@ -0,0 +1,70 @@
+Use foodFeedDB;
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("when sushi is life", "Sushi", "3", "https://1lach6149z9u3oo5y045p0ax-wpengine.netdna-ssl.com/wp-content/uploads/2017/12/Types-of-sushi.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("get you some", "Dim Sum", "7", "https://i.kinja-img.com/gawker-media/image/upload/s--5nVw4IQ9--/c_fill,f_auto,fl_progressive,g_center,h_675,q_80,w_1200/axvyospzf9kaso7542mi.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("burrito is bae", "Mexican", "26", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Burrito_with_rice.jpg/1200px-Burrito_with_rice.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("Always save room for dessert", "Dessert", "17", "https://traveldejavu.com/wp-content/uploads/2017/11/Food-Travel-ultimate-Food-Porn-Internationally-featured.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("pizza diet", "Pizza", "17", "https://slice.seriouseats.com/images/20101115_GoldenBoyClam.JPG");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("Don't mind if I do", "Dessert", "3", "https://pbs.twimg.com/media/C6GgkjWWMAABEsz.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("let's taco bout it", "Mexican", "20", "https://images1.laweekly.com/imager/carne-asada-tacos-at-el-sauz/u/original/4216259/el_sauz.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("wishing this was pizza", "Healthy", "29", "http://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2017/09/main/autumn-glow-salad-1711p85.jpg?itok=xsUVd68s");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("all the carbs🍕", "Pizza", "37", "https://i.pinimg.com/originals/4a/fb/80/4afb807da1a94e4c02d1ef165fc3082a.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("am i skinny yet", "Healthy", "17", "https://hh2983bckk73apigx3cu5b3t23-wpengine.netdna-ssl.com/wp-content/uploads/2015/01/Italian-Salad-featured.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("pasta is for lovers🍝", "Pasta", "3", "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/25025273-175950643005918-348723143679934464-n-1517332976.jpg?crop=1.00xw:0.402xh;0,0.241xh&resize=768:*");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("made me some salmon. the end.", "Healthy", "4", "https://www.collegemagazine.com/wp-content/uploads/2017/03/Screen-Shot-2017-03-15-at-6.53.18-PM.png");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("pretending to be healthy", "Healthy", "9", "https://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("🍕", "Pizza", "7", "http://www.eccopizzeria.co.uk/wp-content/uploads/2015/09/Screen-Shot-2015-09-24-at-6.01.12-PM-e1443114173914.png");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("diet's are for quitters", "Burger", "19", "https://assets.bonappetit.com/photos/57d9d48f9f19b4610e6b6d53/master/w_1200,c_limit/aguynamedpatrick-sandwich-500.jpeg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("🍣,","Sushi", "22", "https://www.getfish.com.au/wp-content/uploads/2017/09/fresh-salmon-sushi-rolls.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("me love sushi", "Sushi", "24", "https://www.mensjournal.com/wp-content/uploads/mf/sushi_main_0.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("carbs for dayz", "Pasta", "25", "https://o.aolcdn.com/images/dims3/GLOB/legacy_thumbnail/1028x675/format/jpg/quality/85/http%3A%2F%2Fo.aolcdn.com%2Fhss%2Fstorage%2Fmidas%2F2a056d6e2a6820147be5f7569e1df3c8%2F0%2Fpasta-with-green-peas-onion-bacon-and-cheese-on-wooden-background-picture-id857931222");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("In-N-Out > Shake Shack", "Burger", "32", "https://cdn.vox-cdn.com/thumbor/DqY1_x9Ww54RWfgsxhvFDxORpC4=/0x0:900x598/1200x800/filters:focal(378x227:522x371)/cdn.vox-cdn.com/uploads/chorus_image/image/57168315/6805312050_d177ea0691_o.0.png");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("🍔  & 🍟 ", "Burger", "33", "https://cdn.vox-cdn.com/thumbor/SA03-2AS45PhL8PIkbW6nj54sjc=/0x0:3712x5568/1200x800/filters:focal(1432x3969:2024x4561)/cdn.vox-cdn.com/uploads/chorus_image/image/56222739/Bacon_Double_Cheeseburger_1_Evan_Sung.0.0.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("post workout treat😋", "Dessert", "31", "https://assets.rbl.ms/13726486/980x.jpg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("Doing what I do best🍽", "Pasta", "38", "http://howyouglow.com/wp-content/uploads/2014/10/image.jpeg");
+
+INSERT INTO Posts (title, category, user_ID, url)
+VALUES ("more meatballs plz", "Pasta", "7", "https://cbsnewyork.files.wordpress.com/2016/01/rubirosa-e1483534730491.png");
